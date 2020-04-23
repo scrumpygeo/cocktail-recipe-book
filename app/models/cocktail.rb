9570ea373c3fc10b1e6f156cd19ea8aa6733bb8b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-23 14:14:47 +0100

    add config for cloudinary

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: {case_sensitive: false }, presence: true
+  has_one_attached :photo
 end
