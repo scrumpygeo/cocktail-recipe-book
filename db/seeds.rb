9f3c307cfef4577d0b8a4ae969e702a8bb884575
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-23 10:12:28 +0100

    add seed file code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Deleting cocktails and doses"
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts "Deleting old ingredients"
+
+Ingredient.destroy_all
+
+
+URL = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(URL).read
+
+ingredients = JSON.parse(serialized_ingredients)
+
+puts "Creating new ingredients..."
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+# extras
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ice")
+
+puts "Created #{Ingredient.count} ingredients."
